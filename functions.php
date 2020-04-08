7f06260c00616199d174cd35733d0a51125a6b29
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-04-08 15:34:22 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -192,3 +192,11 @@ function wp_upe_upgrade_completed( $upgrader_object, $options ) {
  }
 }
 add_action( 'upgrader_process_complete', 'wp_upe_upgrade_completed', 10, 2 );
+
+/*******************************************************************
+Disable Wordpress Update Message
+*******************************************************************/
+if (!current_user_can('edit_users')) {
+  add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
+  add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+}
